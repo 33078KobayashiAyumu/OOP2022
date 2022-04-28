826e2699a00b538711b2cd7eff64cb2cc73db94d
小林歩夢 <33078@ojs.ac.jp>
2022-04-28 15:16:53 +0900

    P71プログラムの改良した

--- Chapter02/SelesCalculator/Program.cs
+++ Chapter02/SelesCalculator/Program.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 namespace SelesCalculator {
     class Program {
         static void Main (string[] args) {
-            SalesCounter sales = new SalesCounter (ReadSales ("sales.csv"));
+
+            SalesCounter sales = new SalesCounter ("sales.csv");
             Dictionary<string, int> amountPerStpre = sales.GetPerStoreSales ();
             foreach (KeyValuePair<string,int> obj in amountPerStpre) {
                 Console.WriteLine ("{0} {1}",obj.Key, obj.Value);
@@ -16,21 +17,6 @@ namespace SelesCalculator {
 
         }
 
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale> ();
-            string[] lines = File.ReadAllLines (filePath);
-            foreach (string line in lines) {
-                String[] items = line.Split (',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse (items[2])
-                };
-                sales.Add (sale);
-            }
 
-            return sales;
-        }
     }
 }
