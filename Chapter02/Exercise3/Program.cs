b5243e972cb080bae763688b7d3e63af0b7340e2
小林歩夢 <33078@ojs.ac.jp>
2022-05-12 15:08:39 +0900

    問題2-3

---
+++ Chapter02/Exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main (string[] args) {
+            var sales = new SalesCounter ("sales.csv");
+            var amountPerStpre = sales.GetPerStoreSales ();
+            foreach (var obj in amountPerStpre) {
+                Console.WriteLine ("{0} {1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
