0038c08c223dc77cffc5bcc5afa61d682f4c897c
小林歩夢 <33078@ojs.ac.jp>
2022-05-12 14:18:10 +0900

    問題2-2

---
+++ ConsoleApp1/Inch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Inch {
+        private const double ratio = 0.0254;//定数ratioを定義
+        //public static readonly double ratio = 0.3048;
+        // メートルからインチを求める(静的メソッド)
+        public static double FromMeter (double meter) {
+            return meter / ratio;
+        }
+
+        // インチからメートルを求める(静的メソッド)
+        public static double ToMeter (double inch) {
+            return inch * ratio;
+        }
+    }
+}
