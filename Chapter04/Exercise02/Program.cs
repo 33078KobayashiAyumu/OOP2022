89dc4d7b9bf6f3539909625f75b796b096e8b7e4
小林歩夢 <33078@ojs.ac.jp>
2022-05-24 15:59:17 +0900

    問題4-2-5答え合わせ済

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,15 +8,75 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main (string[] args) {
+            //4-2-1
+            var ymCollection = new YearMonth[] { 
+                new YearMonth(1999,1),
+                new YearMonth(2000,12),
+                new YearMonth(2001,1),
+                new YearMonth(2022,5),
+                new YearMonth(2101,1),//(P103)最後のカンマはつけておく
+            };
 
 
+            //4-2-2
+            Exercise2_2 (ymCollection);
+            Console.WriteLine ("-------");
 
-            //var ym = new Exercise01.YearMonth (2022, 5);
+            //4-2-4
+            Exercise2_4 (ymCollection);
+            Console.WriteLine ("-------");
 
-            //var check = ym.Is21Century;
-            //var test = ym.AddOneMonth ();
+            //4-2-5
+            Exercise2_5 (ymCollection);
+            Console.WriteLine ("-------");
 
-            //Console.WriteLine (ym);
+            //4-2-6
+            Exercise2_6 (ymCollection);
+        }
+
+        private static void Exercise2_6 (YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection.OrderByDescending(ym => ym.Year)) {
+                Console.WriteLine (ym);
+            }
+        }
+
+
+        //4-2-3
+        //最初に見つかった21世紀のおぶじぇくとを返す
+        //見つからない場合はnullを返す
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+
+            foreach (var ym in yms) {
+                if (ym.Is21Century) {
+                    return ym;
+                }
+            }
+            return null;
+        }
+
+
+        private static void  Exercise2_2 (YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine (ym);
+            }
+
+        }
+
+        private static void Exercise2_4 (YearMonth[] ymCollection) {
+            var yearmonth = FindFirst21C (ymCollection);
+
+            if(yearmonth != null) {
+                Console.WriteLine (yearmonth);
+            }else {
+                Console.WriteLine ("21世紀のデータはありません");
+            }
+        }
+
+        private static void Exercise2_5 (YearMonth[] ymCollection) {
+            var array =  ymCollection.Select (ym => ym.AddOneMonth ()).ToArray();
+            foreach (var ym in array) {
+                Console.WriteLine (ym);
+            }
         }
     }
 }
