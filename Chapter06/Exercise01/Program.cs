302a0206c433c7213cce3ec7a6f8ad980ba5e107
小林歩夢 <33078@ojs.ac.jp>
2022-05-31 15:57:56 +0900

    問題6-1-4まで

---
+++ Chapter06/Exercise01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main (string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1 (numbers);
+            Console.WriteLine ("-----");
+
+            Exercise1_2 (numbers);
+            Console.WriteLine ("-----");
+
+            Exercise1_3 (numbers);
+            Console.WriteLine ("-----");
+
+
+            Exercise1_4 (numbers);
+            Console.WriteLine ("-----");
+
+
+            Exercise1_5 (numbers);
+        }
+
+        private static void Exercise1_1 (int[] numbers) {
+            Console.WriteLine (numbers.Max ());
+        }
+
+        private static void Exercise1_2 (int[] numbers) {
+            foreach (var num in numbers.Skip(numbers.Length - 2)) {
+                Console.WriteLine (num);
+            }
+        }
+
+        private static void Exercise1_3 (int[] numbers) {
+            foreach (var num in numbers.Select(n => n)) {
+                Console.WriteLine (num);
+            }
+        }
+
+        private static void Exercise1_4 (int[] numbers) {
+            foreach (var num in numbers.OrderBy (n => n).Take(3)) {
+                Console.WriteLine (num);
+            }
+        }
+
+        private static void Exercise1_5 (int[] numbers) {
+            
+        }
+    }
+}
