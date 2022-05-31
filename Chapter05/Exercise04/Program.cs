721de601fb766a4880f32bd3f2cdfbaadb1ea50f
小林歩夢 <33078@ojs.ac.jp>
2022-05-31 13:58:41 +0900

    問題5-4解説後

--- Chapter05/Exercise04/Program.cs
+++ Chapter05/Exercise04/Program.cs
@@ -9,12 +9,12 @@ namespace Exercise04 {
         static void Main (string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
-            var rep = line.Replace ("","");
-
-            var words = line.Split (';');
-            foreach (var ln in words) {
-                
+            foreach (var item in line.Split (';')) {
+                var array = item.Split ('=');
+                Console.WriteLine ("{0}:{1}",ToJapanese(array[0]),array[1]);
             }
+
+            
         }
 
         static string ToJapanese (string key) {
