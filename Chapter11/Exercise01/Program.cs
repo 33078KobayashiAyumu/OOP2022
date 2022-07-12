955e3370148cff1d1069491549bcca7c4327272a
小林歩夢 <33078@ojs.ac.jp>
2022-07-12 13:06:50 +0900

    Exercise02途中

--- Chapter11/Exercise01/Program.cs
+++ Chapter11/Exercise01/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Exercise01 {
     class Program {
@@ -19,37 +20,52 @@ namespace Exercise01 {
 
             var newfile = "sports.xml";
             Exercise1_4 (file, newfile);
+
+            //確認
+            var text = File.ReadAllText (newfile);
+            Console.WriteLine (text);
         }
 
         private static void Exercise1_1 (string file) {
-            var xdoc = XDocument.Load ("Sample.xml");
-            var novelists = xdoc.Root.Elements ()
+            var xdoc = XDocument.Load (file);
+            var sport = xdoc.Root.Elements ()
                 .Select (x => new{
                 Name = (string)x.Element ("name"),
                 Member = (int)x.Element ("teammembers")});
-            foreach (var item in novelists) {
+            foreach (var item in sport) {
                 Console.WriteLine ("競技名：{0} 人数：{1}",item.Name,item.Member);
             }
         }
 
         private static void Exercise1_2 (string file) {
-            var xdoc = XDocument.Load ("Sample.xml");
-            var novelists = xdoc.Root.Elements ().Select (x => new {
-                Year = (int)x.Element ("firstplayed"),
-                kanji = x.Element("name").Elements("kanji")
-        });
+            var xdoc = XDocument.Load (file);
+            var sport = xdoc.Root.Elements ().Select (x => new {
+                Firstplayed = x.Element ("firstplayed").Value,
+                Name = x.Element ("name").Attribute ("kanji").Value
+            }).OrderBy (x => int.Parse (x.Firstplayed));
             
-            foreach (var item in novelists) {
-                Console.WriteLine ("{0}",item);
+            foreach (var item in sport) {
+                Console.WriteLine ("{0}{1}", item.Name, item.Firstplayed);
             }
         }
 
         private static void Exercise1_3 (string file) {
-            
+            var xdoc = XDocument.Load (file);
+            var sport = xdoc.Root.Elements ().Select (x => new {
+                Name = x.Element ("name").Value,
+                Menber = x.Element ("teammembers").Value
+            })  .OrderByDescending (x => int.Parse(x.Menber)).First();
+                Console.WriteLine ("{0}{1}",sport.Name,sport.Menber);
         }
 
         private static void Exercise1_4 (string file, string newfile) {
-            
+            var element = new XElement ("ballsport",
+                          new XElement ("name", "サッカー", new XAttribute ("kanji", "蹴球")),
+                          new XElement ("teammembers", "11"),
+                          new XElement ("firstplayed", "1863"));
+            var xdoc = XDocument.Load (file);
+            xdoc.Root.Add (element);
+            xdoc.Save (newfile);
         }
     }
 }
---
+++ Chapter12/Exercise01/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Exercise01 {
+
+
+    public class Employee {
+        [XmlElement (ElementName = "id")]
+        public int Id { get; set; }
+        [XmlElement (ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement (ElementName = "hiredate")]
+        public DateTime HireDate { get; set; }
+        public override string ToString () {
+            return string.Format ("[Id={0}, Name={1}, HireDate={2}]",
+                                  Id, Name, HireDate);
+        }
+    }
+    [DataContract (Name = "employee2")]
+    public class Employee1 {
+
+        public int Id { get; set; }
+        [DataMember (Name = "name")]
+        public string Name { get; set; }
+        [DataMember (Name = "hireDate")]
+        public DateTime HireDate { get; set; }
+
+    }
+
+
+    class Program {
+        static void Main (string[] args) {
+
+            Exercise1_1 ("employee.xml");
+
+            //// これは確認用
+            //Console.WriteLine (File.ReadAllText ("employee.xml"));
+            //Console.WriteLine ();
+
+            Exercise1_2 ("employees.xml");
+            Exercise1_3 ("employees.xml");
+            //Console.WriteLine ();
+
+            Exercise1_4 ("employees.json");
+
+            //// これは確認用
+            //Console.WriteLine (File.ReadAllText ("employees.json"));
+        }
+
+        private static void Exercise1_1 (string outfile) {
+            var emp = new Employee {
+
+                Id = 123,
+                Name = "a",
+                HireDate = new DateTime (2022, 2, 22),
+            };
+            using (var writer = XmlWriter.Create ("employee.xml")) {
+                var serializer = new XmlSerializer (emp.GetType ());
+                serializer.Serialize (writer, emp);
+            }
+            Display ("employee.xml");
+        }
+
+        private static void Exercise1_2 (string v) {
+            var emp = new Employee[] {
+               new Employee {
+                  Id = 123,
+                  Name = "あ",
+                  HireDate = new DateTime (2022, 2, 22),
+              },
+               new Employee {
+                  Id = 1234,
+                  Name = "ああ",
+                  HireDate = new DateTime (2011, 1, 11),
+               },
+            };
+            
+            using (var writer = XmlWriter.Create ("employees.xml")) {
+                var serializer = new DataContractSerializer (emp.GetType ());
+                serializer.WriteObject (writer, emp);
+            }
+                Display ("employees.xml");
+        }
+
+        private static void Exercise1_3 (string v) {
+            using (var reader = XmlReader.Create ("employees.xml")) {
+                var serializer = new DataContractSerializer (typeof (Employee[]));
+                var emp = serializer.ReadObject (reader) as Employee[];
+                foreach (var item in emp) {
+                    Console.WriteLine ("{0}{1}{2}",item.Name,item.Id,item.HireDate);
+                }
+            }
+        }
+
+        private static void Exercise1_4 (string v) {
+
+            var emp = new Employee1[] {
+               new Employee1 {
+                  Id = 123,
+                  Name = "あ",
+                  HireDate = new DateTime (2022, 2, 22),
+              },
+               new Employee1 {
+                  Id = 1234,
+                  Name = "ああ",
+                  HireDate = new DateTime (2011, 1, 11),
+               }
+            };
+
+            using (var writer = new FileStream ("employees2.json", FileMode.Create, FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer (emp.GetType (),new DataContractJsonSerializerSettings {
+                    DateTimeFormat = new DateTimeFormat("yyyy-MM-dd")
+                });
+                serializer.WriteObject (writer, emp);
+            }
+            Display ("employees2.json");
+        }
+        //XMLファイルの中身表示用
+        private static void Display (string filename) {
+            var lines = File.ReadLines (filename);
+            foreach (var line in lines)
+                Console.WriteLine (line);
+        }
+    }
+}
