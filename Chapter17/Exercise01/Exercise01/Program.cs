14d54b1a6bc4a2464fd320eaace11475d5164269
小林歩夢 <33078@ojs.ac.jp>
2022-08-30 13:13:13 +0900

    8/29

--- Chapter17/Exercise01/Exercise01/Program.cs
+++ Chapter17/Exercise01/Exercise01/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextFileProcessor;
 
-namespace TextFileProcessor {
+namespace Exercise01 {
     class Program {
         static void Main (string[] args) {
+            TextProcessor.Run<ToLower> (@"C:\Users\infosys\source\repos\OOP2022\Chapter17\Exercise01\Sample.txt");
         }
     }
 }
